6bae4e25d6428bff8fb8fbb6ea5af690efe3938b
LamidonMatthieu <matthieu.lamidon@reseau.eseo.fr>
2025-10-07 13:17:42 +0200

    patch a lot of probleme in compilation

--- src/index.ts
+++ src/index.ts
@@ -31,5 +31,5 @@ app.post("/posts", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on http://localhost:${PORT} `);
 });
